94a25f6e76a77ed92fb0834d3c343dcb4afba370
Dawid Kurpiel <kurpieldawid@gmail.com>
2020-08-31 23:21:43 +0200

    Fixed bug

--- server/index.js
+++ server/index.js
@@ -27,10 +27,11 @@ const connect = mongoose
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  // });
 }
+
 app.use(jwt());
 
 app.use("/api/books", require("./routes/books.controller"));
